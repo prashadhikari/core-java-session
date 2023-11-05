e7bfb47acecc3f9e25cc7db5d8bc69db69381216
Prashant Adhikari <prash.adhikari135@gmail.com>
2023-11-05 17:50:47 -0500

    OOP concept of states and behavior

---
+++ src/oopconcept/BankAccount.java
@@ -0,0 +1,39 @@
+package oopconcept;
+
+public class BankAccount {
+
+    //States or instance variables
+    /*
+        account number
+        account type
+        routing number
+        account holder name
+        account balance
+        account holder address
+        account holder signature
+        social security number
+     */
+    //Constructors
+    /*
+       public BankAccount(){
+
+       }
+
+       public BankAccount(String accountNumber){
+
+       }
+     */
+
+    //Behaviors or functions or methods
+    /*
+        check balance
+        transfer balance
+        pay bills
+        deposit balance
+
+        public void checkBalance(){
+            // code
+        }
+
+     */
+}
---
+++ src/oopconcept/FacebookPost.java
@@ -0,0 +1,35 @@
+package oopconcept;
+
+public class FacebookPost {
+
+    // states
+    /*
+        post type
+        post creator
+        post created date
+        post updated date
+        post content
+        post reactions
+        post location
+        post tag
+        post share link
+        post visibility
+        post comments
+     */
+
+    // constructor
+
+    // behavior
+    /*
+        add photos
+        add videos
+        add locations
+        tag users
+        publish post
+        edit post
+        delete post
+        share post
+        like post
+        react on post
+     */
+}
